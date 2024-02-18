135e349a4ca861a5b7f5b46e80f5bb9d431387f4
Serenity smith <serenitysmithrn@hotmail.com>
2024-02-18 11:35:51 -0500

    Add Firebase Storage configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,6 +5,9 @@ import "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+import { getStorage } from "firebase/storage";
+
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC_2Jv_BZrhnOeTJM7RsVVo3dl6kKkSo_I",
@@ -15,6 +18,33 @@ const firebaseConfig = {
   appId: "1:577859285076:web:79222e86277655f43b7dc1",
   measurementId: "G-ZBH6TZN8RE"
 };
+
+const storage = new Storage();
+
+//-
+// Make a bucket's contents publicly readable.
+//-
+const myBucket = storage.bucket('https://console.firebase.google.com/project/booktokapi/storage/booktokapi.appspot.com/files');
+
+const options = {
+  entity: 'allUsers',
+  role: storage.acl.READER_ROLE
+};
+
+myBucket.acl.add(options, function(err, aclObject) {});
+
+//-
+// If the callback is omitted, we'll return a Promise.
+//-
+myBucket.acl.add(options).then(function(data) {
+  const aclObject = data[0];
+  const apiResponse = data[1];
+});
+
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
