b8743c5d5d989bf4eb8461dbed5aa86d1167e33e
Serenity smith <serenitysmithrn@hotmail.com>
2024-02-19 05:11:54 -0500

    Add Community page and update navigation

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,65 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import BookList from '../../components/BookList'; // Make sure to update the import path
 
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import './style.css';
-import BookList from '../../components/BookList'
-function BookList (props) {
-
-  
-  <div className="book-list">
-    {props.books && ((props.books.totalItems > 0)
-      ?
-      (<Row xs={1} md={4} className="book-list">
-        {props.books.items.map((item, index) => {
-          return (
-            <Col key={index}>
-              <BookList item={item} />
-            </Col>)
-}) }
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [books, setBooks] = useState([]);
-  
-  
 
-           
-          </Row>): <div className="prompt">No results, try a different term!</div>)}
-          
-         </div>}   
-        
-  async function getBooks(book) {
+  const getBooks = async (book) => {
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}`);
     const data = await response.json();
     return data;
-  }
-
-  const extractThumbnail = ({ imageLinks }) => {
-    const DEFAULT_THUMBNAIL = "icons/logo.svg";
-    if (!imageLinks || !imageLinks.thumbnail) {
-      return DEFAULT_THUMBNAIL;
-    }
-    return imageLinks.thumbnail.replace("http://", "https://");
-  };
-
-  const drawBookList = (bookData) => {
-    return bookData.items.map(({ volumeInfo }) => (
-      <div className="App-search-book-list" key={volumeInfo.title}>
-        <a href={volumeInfo.previewLink} target='_blank' rel="noopener noreferrer">
-          <img className='thumbnail' src={extractThumbnail(volumeInfo)} alt='cover' />
-        </a>
-        <div className="App-search-book-list">
-          <h3 className='book-title'>
-            <a href={volumeInfo.previewLink} target='_blank' rel="noopener noreferrer">
-              {volumeInfo.title}
-            </a>
-          </h3>
-          <div className='App-search-book-list'>{volumeInfo.authors}</div>
-          <div className='App-search-book-list'>{volumeInfo.categories || "Others"}</div>
-        </div>
-      </div>
-    ));
   };
 
   const drawListBook = async () => {
@@ -70,7 +20,9 @@ const Search = () => {
       } else if (data.totalItems === 0) {
         setBooks([<div className='prompt'>ツ No results, try a different term!</div>]);
       } else {
-        setBooks(drawBookList(data));
+        setBooks(data.items.map((item, index) => (
+          <BookList key={index} item={item} />
+        )));
       }
     } else {
       setBooks([]);
@@ -103,13 +55,6 @@ const Search = () => {
         </Button>
       </Form>
 
-      <input
-        type='text'
-        id='search-box'
-        value={searchTerm}
-        onChange={handleChange}
-        placeholder='Search books...'
-      />
       <div className='search'>{books}</div>
     </div>
   );
